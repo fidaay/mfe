041e1eff4b8c35ad7fd53482093c37f1aa972c7b
fidaay <guiidolopez@hotmail.com>
2021-06-23 18:05:15 -0600

    Last commit before uploading the repo to github

--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { App } from './App';
 
 // Mount function to start up the app
 const mount = (el) => {
-  ReactDOM.render(<h1>Hola!</h1>, el);
+  ReactDOM.render(<App />, el);
 };
 // If we are in development and in isolation, call mount immediately
 if (process.env.NODE_ENV === 'development') {
@@ -13,4 +14,6 @@ if (process.env.NODE_ENV === 'development') {
     mount(devRoot);
   }
 }
+
 // We are running thorugh container and we should export the mount function
+export { mount };
